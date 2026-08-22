8f00c24996952e27b6aec372d6bda9cceede49ae
Ana Júlia <anaju.aoliveiraa@gmail.com>
2026-08-22 13:01:38 -0300

    mudanças janela no navegador

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './assets/global.css';
 import { AppRouter } from './routes/AppRouter.jsx';
 import { AuthProvider } from './contexts/AuthContext.jsx';
+import logo from './assets/logo_window.png';
+
+document.getElementById('favicon').setAttribute('href', logo);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
